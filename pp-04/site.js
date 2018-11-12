68d0ba658365681176657995d5f0c2859c314909
Kem <Treehouse@localhost.localdomain>
2018-11-12 01:56:53 -0600

    Messed around with site.js. Updated audio tags in html

--- pp-04/site.js
+++ pp-04/site.js
@@ -2,3 +2,5 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+document.querySelector("audioplay").addEventListener("click,ringDoorbell")
+document.querySelector("#doorbell").innerHTML = ""
